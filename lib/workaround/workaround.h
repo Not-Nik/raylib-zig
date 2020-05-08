b611ac7a2cc4e478cc4cb6b79d849e34aa1787b4
Not-Nik <nik.wipper@gmx.de>
2020-05-08 20:03:42 +1000

    Remove unnecessary defines

--- lib/workaround/workaround.h
+++ lib/workaround/workaround.h
@@ -5,10 +5,6 @@
 
 #include <raylib.h>
 
-#define VECTOR2 float x, float y
-#define VECTOR3 float x, float y, float z
-#define COLOR unsigned char r, unsigned char g, unsigned char b, unsigned char a
-
 Ray WGetMouseRay(Vector2 * mousePosition, Camera camera);
 
 void WDrawSphere(Vector3 * centerPos, float radius, Color * color);
