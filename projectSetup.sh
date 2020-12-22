e47623000d975eb76a032f6a6673f92feb2568d5
Not-Nik <nik.wipper@gmx.de>
2020-12-22 15:58:39 +0100

    Copy build system from master (1/?)

--- projectSetup.sh
+++ projectSetup.sh
@@ -6,26 +6,35 @@ fi
 
 mkdir "$PROJECT_NAME" && cd "$PROJECT_NAME" || exit
 touch build.zig
+echo "generating project files..."
+echo 'const std = @import("std");
+const Builder = std.build.Builder;
+const raylib = @import("raylib-zig/lib.zig").Pkg("raylib-zig"); //call .Pkg() with the folder raylib-zig is in relative to project build.zig
 
-echo 'const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("'"$PROJECT_NAME"'", "src/main.zig");
-    exe.setBuildMode(mode);
-    exe.linkSystemLibrary("raylib");
-    exe.addCSourceFile("raylib-zig/workaround.c", &[_][]const u8{});
-    exe.addPackagePath("raylib", "raylib-zig/raylib-zig.zig");
-    exe.addPackagePath("raylib-math", "lib/raylib-zig-math.zig");
+    const target = b.standardTargetOptions(.{});
 
-    const runExe = exe.run();
-    const exeStep = b.step("run", "Runs the app");
-    exeStep.dependOn(&runExe.step);
+    const system_lib = b.option(bool, "system-raylib", "link to preinstalled raylib libraries") orelse false;
+
+    const exe = b.addExecutable("'$PROJECT_NAME'", "src/main.zig");
+    exe.setBuildMode(mode);
+    exe.setTarget(target);
+
+    raylib.link(exe, system_lib);
+    raylib.addAsPackage("raylib", exe);
+    raylib.math.addAsPackage("raylib-math", exe);
+
+    const run_cmd = exe.run();
+    const run_step = b.step("run", "run '$PROJECT_NAME'");
+    run_step.dependOn(&run_cmd.step);
+
+    exe.install();
 }
 ' >> build.zig
 
 mkdir src
-mkdir raylib-zig
-cp ../lib/** raylib-zig
-cp ../examples/core/basic_window.zig src
-mv src/basic_window.zig src/main.zig
+cp ../examples/core/basic_window.zig src/main.zig
+echo "cloning raylib-zig inside of project..."
+git clone ../ raylib-zig
