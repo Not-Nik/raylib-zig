d76c85d71ac0639f8ff59be4017a49b510c4d09d
KeanBuyst <keanbuyst@gmail.com>
2024-04-07 13:58:02 +0200

    changed to [*c]const

--- lib/raylib-zig.zig
+++ lib/raylib-zig.zig
@@ -1386,20 +1386,20 @@ pub fn loadMusicStreamFromMemory(fileType: [:0]const u8, data: []const u8) Music
     return cdef.LoadMusicStreamFromMemory(@as([*c]const u8, @ptrCast(fileType)), @as([*c]const u8, @ptrCast(data)), @as(c_int, @intCast(data.len)));
 }
 
-pub fn drawLineStrip(points: []Vector2, color: Color) void {
-    cdef.DrawLineStrip(@as([*c]Vector2, @ptrCast(points)), @as(c_int, @intCast(points.len)), color);
+pub fn drawLineStrip(points: []const Vector2, color: Color) void {
+    cdef.DrawLineStrip(@as([*c]const Vector2, @ptrCast(points)), @as(c_int, @intCast(points.len)), color);
 }
 
-pub fn drawTriangleFan(points: []Vector2, color: Color) void {
-    cdef.DrawTriangleFan(@as([*c]Vector2, @ptrCast(points)), @as(c_int, @intCast(points.len)), color);
+pub fn drawTriangleFan(points: []const Vector2, color: Color) void {
+    cdef.DrawTriangleFan(@as([*c]const Vector2, @ptrCast(points)), @as(c_int, @intCast(points.len)), color);
 }
 
-pub fn drawTriangleStrip(points: []Vector2, color: Color) void {
-    cdef.DrawTriangleStrip(@as([*c]Vector2, @ptrCast(points)), @as(c_int, @intCast(points.len)), color);
+pub fn drawTriangleStrip(points: []const Vector2, color: Color) void {
+    cdef.DrawTriangleStrip(@as([*c]const Vector2, @ptrCast(points)), @as(c_int, @intCast(points.len)), color);
 }
 
-pub fn checkCollisionPointPoly(point: Vector2, points: []Vector2) bool {
-    return cdef.CheckCollisionPointPoly(point, @as([*c]Vector2, @ptrCast(points)), @as(c_int, @intCast(points.len)));
+pub fn checkCollisionPointPoly(point: Vector2, points: []const Vector2) bool {
+    return cdef.CheckCollisionPointPoly(point, @as([*c]const Vector2, @ptrCast(points)), @as(c_int, @intCast(points.len)));
 }
 
 pub fn loadFontEx(fileName: [:0]const u8, fontSize: i32, fontChars: []i32) Font {
