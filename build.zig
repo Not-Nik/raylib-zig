a08297ee423fb94fa98060e452c9b4dcae5fc9ce
Leonardo <leonardo@kreienbuehl.net>
2024-08-07 02:58:02 +0100

    added 2d camera mouse zoom example

--- build.zig
+++ build.zig
@@ -39,13 +39,7 @@ const Program = struct {
     desc: []const u8,
 };
 
-fn link(
-    b: *std.Build,
-    exe: *std.Build.Step.Compile,
-    target: std.Build.ResolvedTarget,
-    optimize: std.builtin.Mode,
-    options: Options
-) void {
+fn link(b: *std.Build, exe: *std.Build.Step.Compile, target: std.Build.ResolvedTarget, optimize: std.builtin.Mode, options: Options) void {
     const lib = getRaylib(b, target, optimize, options);
 
     const target_os = exe.rootModuleTarget().os.tag;
@@ -93,19 +87,7 @@ fn link(
 var _raylib_lib_cache: ?*std.Build.Step.Compile = null;
 fn getRaylib(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.Mode, options: Options) *std.Build.Step.Compile {
     if (_raylib_lib_cache) |lib| return lib else {
-        const raylib = b.dependency("raylib", .{
-            .target = target,
-            .optimize = optimize,
-            .raudio = options.raudio,
-            .rmodels = options.rmodels,
-            .rshapes = options.rshapes,
-            .rtext = options.rtext,
-            .rtextures = options.rtextures,
-            .platform_drm = options.platform_drm,
-            .shared = options.shared,
-            .linux_display_backend = options.linux_display_backend,
-            .opengl_version = options.opengl_version
-        });
+        const raylib = b.dependency("raylib", .{ .target = target, .optimize = optimize, .raudio = options.raudio, .rmodels = options.rmodels, .rshapes = options.rshapes, .rtext = options.rtext, .rtextures = options.rtextures, .platform_drm = options.platform_drm, .shared = options.shared, .linux_display_backend = options.linux_display_backend, .opengl_version = options.opengl_version });
 
         const lib = raylib.artifact("raylib");
 
@@ -118,12 +100,15 @@ fn getRaylib(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.buil
         lib.step.dependOn(&gen_step.step);
 
         const raygui_c_path = gen_step.add("raygui.c", "#define RAYGUI_IMPLEMENTATION\n#include \"raygui.h\"\n");
-        lib.addCSourceFile(.{ .file = raygui_c_path, .flags = &[_][]const u8{
-            "-std=gnu99",
-            "-D_GNU_SOURCE",
-            "-DGL_SILENCE_DEPRECATION=199309L",
-            "-fno-sanitize=undefined", // https://github.com/raysan5/raylib/issues/3674
-        }});
+        lib.addCSourceFile(.{
+            .file = raygui_c_path,
+            .flags = &[_][]const u8{
+                "-std=gnu99",
+                "-D_GNU_SOURCE",
+                "-DGL_SILENCE_DEPRECATION=199309L",
+                "-fno-sanitize=undefined", // https://github.com/raysan5/raylib/issues/3674
+            },
+        });
         lib.addIncludePath(raylib.path("src"));
         lib.addIncludePath(raygui_dep.path("src"));
 
@@ -215,6 +200,11 @@ pub fn build(b: *std.Build) !void {
             .name = "2d_camera",
             .path = "examples/core/2d_camera.zig",
             .desc = "Shows the functionality of a 2D camera",
+        },
+        .{
+            .name = "2d_camera_mouse_zoom",
+            .path = "examples/core/2d_camera_mouse_zoom.zig",
+            .desc = "Shows mouse zoom demo",
         },
         .{
             .name = "3d_camera_first_person",
